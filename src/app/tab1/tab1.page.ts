25704f41913096b91fb3bcfd897f549564088432
shinuamai <laurad00sandovalg@gmail.com>
2023-08-29 16:12:40 -0500

    a big stage

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { ModalController } from '@ionic/angular'; // Importa ModalController
+import { ModalComponent } from '../modal/modal.component'; // Asegúrate de importar el componente modal
+ 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  isModalOpen = false;
 
-  constructor() {}
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+  constructor(private modalController: ModalController) {} 
+
+  async openModal(id: number) {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      componentProps: {
+        modalId: id,
+      },
+    });
+    return await modal.present();
+  }
 
 }
